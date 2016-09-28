d1676cb29054703a6f2c9a07ba62b7b2648e6ea8
Leone Parise <leone.parise@gmail.com>
2016-09-28 15:40:25 -0300

    Fix project structure

--- DAKeyboardControlExample/DAKeyboardControl/DAKeyboardControl.h
+++ DAKeyboardControlExample/DAKeyboardControl/DAKeyboardControl.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double DAKeyboardControlVersionNumber;
 FOUNDATION_EXPORT const unsigned char DAKeyboardControlVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DAKeyboardControl/PublicHeader.h>
-
-
