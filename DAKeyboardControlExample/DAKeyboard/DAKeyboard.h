e88b56edf0736509bffd566e254d73c5b85a981d
Leone Parise <leone.parise@gmail.com>
2016-09-28 16:08:39 -0300

    Fix Carthage support

---
+++ DAKeyboardControlExample/DAKeyboard/DAKeyboard.h
@@ -0,0 +1,19 @@
+//
+//  DAKeyboard.h
+//  DAKeyboard
+//
+//  Created by Leone Parise Vieira da Silva on 28/09/16.
+//  Copyright © 2016 Shout Messenger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DAKeyboard.
+FOUNDATION_EXPORT double DAKeyboardVersionNumber;
+
+//! Project version string for DAKeyboard.
+FOUNDATION_EXPORT const unsigned char DAKeyboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DAKeyboard/PublicHeader.h>
+
+#import "DAKeyboardControl.h"
