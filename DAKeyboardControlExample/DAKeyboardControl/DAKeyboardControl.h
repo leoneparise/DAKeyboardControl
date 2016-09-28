a331509ac0447dde8686a99e0b27c2d9892bbe82
Leone Parise <leone.parise@gmail.com>
2016-09-28 15:23:19 -0300

    Add carthage support and swift 2.3

---
+++ DAKeyboardControlExample/DAKeyboardControl/DAKeyboardControl.h
@@ -0,0 +1,19 @@
+//
+//  DAKeyboardControl.h
+//  DAKeyboardControl
+//
+//  Created by Leone Parise Vieira da Silva on 28/09/16.
+//  Copyright © 2016 Shout Messenger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DAKeyboardControl.
+FOUNDATION_EXPORT double DAKeyboardControlVersionNumber;
+
+//! Project version string for DAKeyboardControl.
+FOUNDATION_EXPORT const unsigned char DAKeyboardControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DAKeyboardControl/PublicHeader.h>
+
+
